7f059c093d48eec38b0378bd7036699073a9eed6
zeckauh <480220726@qq.com>
2024-07-08 10:18:51 +0800

    创建项目

---
+++ src/main/java/com/zzhedu/springwebquickstart02/SpringWebQuickstart02Application.java
@@ -0,0 +1,13 @@
+package com.zzhedu.springwebquickstart02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebQuickstart02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebQuickstart02Application.class, args);
+	}
+
+}
